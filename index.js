30e2f4e8421238a910f610a9b7ea1f371f0970df
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-06-22 20:41:43 +0200

    done task section, catagory buttons

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ class App {
     input: document.querySelector(".app__add-task__input"),
     tasksList: document.querySelector(".app__tasks-list"),
     addTaskBtn: document.querySelector(".app__add-task__btn"),
+    catagoryButtons: document.querySelectorAll(".catagory__button"),
   };
   tasksArr = [];
+  taskArrDone = [];
 
   constructor() {
     this.init();
@@ -15,13 +17,20 @@ class App {
     //
     this.htmlElements.taskList.addEventListener("click", (e) => {
       let index = e.target.dataset.index;
-      if (e.target.classList.contains("buttons__delete-button")) {
+      if (e.target.parentNode.classList.contains("buttons__delete-button")) {
         this.deleteTask(index);
+      }
+      if (e.target.parentNode.classList.contains("buttons__check-button")) {
+        this.ItIsDone(e);
       }
     });
     this.htmlElements.addTaskBtn.addEventListener("click", () => {
       this.addTask();
       this.renderTasks();
+    });
+    // define catagory Listener
+    this.htmlElements.catagoryButtons.forEach((catagoryButton) => {
+      catagoryButton.addEventListener("click", (e) => this.defineCategory(e));
     });
   }
 
@@ -38,11 +47,15 @@ class App {
 
   createTaskElement(taskText, index) {
     const taskHtlm = `<div class="tasks-list__task .task">
-          <span class="task__index">${index + 1}</span
+          <span class="task__index"><img src="sources/img/icons/${this.categoryActive}-icon.svg" alt="" /></span
           ><span class="task__text">${taskText}</span>
           <div class="task__buttons buttons">
-            <button class="buttons__check-button button"data-index="${index}">L</button>
-            <button class="buttons__delete-button button"data-index="${index}">X</button>
+            <button class="buttons__check-button button"data-index="${index}"><img class="button-img" src="sources/img/check-icon.svg" alt="" /></button>
+            <button class="buttons__delete-button button"data-index="${index}"> <img
+            class="button-img"
+            src="sources/img/delete-icon.svg"
+            alt=""
+          /></button>
           </div>
         </div>`;
     return taskHtlm;
@@ -61,6 +74,21 @@ class App {
   deleteTask(indexOfTask) {
     this.tasksArr.splice(indexOfTask, 1);
     this.renderTasks();
+  }
+
+  ItIsDone(e) {
+    let currentTask = e.target.parentNode.parentNode.parentNode.cloneNode(true);
+    let taskIndex = e.target.parentNode;
+    this.deleteTask(taskIndex);
+    console.log(taskIndex);
+    const doneList = document.querySelector(".tasks-list--done");
+    doneList.insertAdjacentElement("beforeend", currentTask);
+  }
+  defineCategory(e) {
+    console.log(e.target.src);
+
+    this.categoryActive = e.target.parentNode.dataset.category;
+    console.log(this.categoryActive);
   }
 }
 
