f892a28f632b5689e08e89461fcfb79dcaf25834
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-06-23 14:14:51 +0200

    delete from done stage som style

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class App {
   htmlElements = {
     taskList: document.querySelector(".app__tasks-list"),
+    doneTaskList: document.querySelector(".tasks-list--done"),
     input: document.querySelector(".app__add-task__input"),
     tasksList: document.querySelector(".app__tasks-list"),
     addTaskBtn: document.querySelector(".app__add-task__btn"),
@@ -8,15 +9,16 @@ class App {
   };
   tasksArr = [];
   taskArrDone = [];
+  categoryActive;
 
   constructor() {
     this.init();
   }
 
   init() {
-    //
+    // removing from task list
     this.htmlElements.taskList.addEventListener("click", (e) => {
-      let index = e.target.dataset.index;
+      let index = e.target.parentNode.dataset.index;
       if (e.target.parentNode.classList.contains("buttons__delete-button")) {
         this.deleteTask(index);
       }
@@ -25,29 +27,44 @@ class App {
       }
     });
     this.htmlElements.addTaskBtn.addEventListener("click", () => {
-      this.addTask();
+      this.createTask();
       this.renderTasks();
     });
     // define catagory Listener
     this.htmlElements.catagoryButtons.forEach((catagoryButton) => {
       catagoryButton.addEventListener("click", (e) => this.defineCategory(e));
     });
+    //removing from done List
+    this.htmlElements.doneTaskList.addEventListener("click", (e) => {
+      let index = e.target.parentNode.dataset.index;
+      if (e.target.parentNode.classList.contains("buttons__delete-button")) {
+        this.deleteDoneTask(index);
+      }
+      if (e.target.parentNode.classList.contains("buttons__check-button")) {
+        this.ItIsDone(e);
+      }
+    });
   }
 
-  // Add task to taskArr
-  addTask() {
-    let taskText = this.htmlElements.input.value;
-    this.tasksArr.push(this.upperCaseFirstLeater(taskText));
-    this.htmlElements.input.value = "";
-  }
   // Upercase First Leater of task
   upperCaseFirstLeater(task) {
     return task.charAt(0).toUpperCase() + task.slice(1);
   }
+  createTask() {
+    let task = {
+      taskText: this.upperCaseFirstLeater(this.htmlElements.input.value),
+      category: this.categoryActive,
+    };
+    if (task.taskText == "") return;
 
-  createTaskElement(taskText, index) {
+    this.htmlElements.input.value = "";
+    this.tasksArr.push(task);
+    console.log(this.tasksArr);
+  }
+
+  createTaskElement(taskText, category = "work", index) {
     const taskHtlm = `<div class="tasks-list__task .task">
-          <span class="task__index"><img src="sources/img/icons/${this.categoryActive}-icon.svg" alt="" /></span
+          <span class="task__index"><img src="sources/img/icons/${category}-icon.svg" alt="" /></span
           ><span class="task__text">${taskText}</span>
           <div class="task__buttons buttons">
             <button class="buttons__check-button button"data-index="${index}"><img class="button-img" src="sources/img/check-icon.svg" alt="" /></button>
@@ -66,7 +83,7 @@ class App {
     this.tasksArr.forEach((task, index) => {
       this.htmlElements.tasksList.insertAdjacentHTML(
         "beforeend",
-        this.createTaskElement(task, index)
+        this.createTaskElement(task.taskText, task.category, index)
       );
     });
   }
@@ -75,20 +92,24 @@ class App {
     this.tasksArr.splice(indexOfTask, 1);
     this.renderTasks();
   }
+  deleteDoneTask(indexOfTask) {
+    console.log(this.taskArrDone);
+
+    this.taskArrDone.splice(indexOfTask, 1);
+    this.htmlElements.doneTaskList.textContent = "";
+    this.taskArrDone.forEach((el) => this.htmlElements.doneTaskList.append(el));
+  }
 
   ItIsDone(e) {
     let currentTask = e.target.parentNode.parentNode.parentNode.cloneNode(true);
+    this.taskArrDone.push(currentTask);
     let taskIndex = e.target.parentNode;
     this.deleteTask(taskIndex);
-    console.log(taskIndex);
     const doneList = document.querySelector(".tasks-list--done");
     doneList.insertAdjacentElement("beforeend", currentTask);
   }
   defineCategory(e) {
-    console.log(e.target.src);
-
     this.categoryActive = e.target.parentNode.dataset.category;
-    console.log(this.categoryActive);
   }
 }
 
